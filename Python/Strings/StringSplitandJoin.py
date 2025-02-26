a20a26bb193dabe2d2bc3f8aa3d6689e3ce30dab
Harshil <harshilmayani108@gmail.com>
2025-02-26 21:08:49 +0530

    new commit

---
+++ Python/Strings/StringSplitandJoin.py
@@ -0,0 +1,13 @@
+
+
+def split_and_join(line):
+    # write your code here
+    line = line.split()
+    result = "-".join(line)
+    
+    return result
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
